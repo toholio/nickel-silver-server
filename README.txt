42cf20d75a76f74f29b8304e451410c1cfcbb475
toholio <toholio@26e8b423-a61e-4a5e-a1cf-e454a07ad340>
2008-03-19 07:34:12 +0000

    Update documentation and history in preparation for the first 'public' release.
    git-svn-id: http://nickel-silver.rubyforge.org/svn/nickel-silver-server/trunk@8 26e8b423-a61e-4a5e-a1cf-e454a07ad340

--- README.txt
+++ README.txt
@@ -4,29 +4,31 @@
 
 == DESCRIPTION:
 
-Ruby, literally, on rails! A Ruby implementation of the LocoNetOverTCP protocol allowing remote clients to connect to Digitrax based model railway layouts. Also included are a number of tools for LocoNet logging and locomotive control.
+A Ruby server implementing the LocoNetOverTCP protocol allowing remote clients to connect to Digitrax based model railway layouts. Currently supports version 1 of the protocol.
 
 == FEATURES/PROBLEMS:
 
 * Complete support for LocoNetOverTCP version 1
+* Multithreaded server based on GServer
+* Easily extended to use new hardware interfaces
 
 == SYNOPSIS:
 
   require 'rubygems'
-	require 'nickel-silver-server'
+  require 'nickel-silver-server'
   
-	# connect to a LocoBufferUSB on the virtual serial port /dev/tty.serialport
-	interface = LocoBufferUSB.new( '/dev/tty.serialport' )
+  # connect to a LocoBufferUSB on the virtual serial port /dev/tty.serialport
+  interface = LocoBufferUSB.new( '/dev/tty.serialport' )
   
-	# create a server using the default port (i.e. 5626, 'loco' spelt on a phone keypad)
-	# using our freshly connected LocoBuffer-USB
-	server = LocoNetServer.new( interface )
+  # create a server using the default port (i.e. 5626, 'loco' spelt on a phone keypad)
+  # using our freshly connected LocoBuffer-USB
+  server = LocoNetServer.new( interface )
   
-	# start the server
-	server.start
+  # start the server
+  server.start
   
-	# wait for the server to stop before exiting
-	server.join
+  # wait for the server to stop before exiting
+  server.join
 
 == REQUIREMENTS:
 
--- lib/nickel-silver-server.rb
+++ lib/nickel-silver-server.rb
@@ -5,12 +5,58 @@ require 'loconetserver'
 
 module NickelSilver
   module Server
-    # nothing here yet
-    # all the action is in LocoNetServer.rb
     
+    # An interface class should provide the following:
+    # * Store incoming packets as FixNums representing bytes in a buffer array
+    # * Send outgoing bytes (represented as FixNums in a buffer array) to LocoNet
+    # * Use a Mutex to lock access to the buffers when in use (remember Nickel-Silver is multithreaded)
+    # * Provide a method that causes your interface to start collecting packets
+    #
+    # The interface is simple. Only the following public methods are needed:
+    # * Accessors for input_buffer, output_buffer and io_mutex
+    # * run() which starts buffering
+    #
+    # How you do this will depend upon your hardware. Take a look at the LocoBufferUSB class to get
+    # an idea of how it might be done.
+    #
+    # A stub driver might look like the following...
+    #
+    #   class SomeLocoNetInterface
+    #     attr_accessor :input_buffer, :output_buffer, :io_mutex
+    #   
+    #     def initialize
+    #       # these may be modified at any time by the server
+    #       @input_buffer = []
+    #       @output_buffer = []
+    #   
+    #       # only make changes when locked using @io_mutex
+    #       @io_mutex = Mutex.new
+    #     end
+    #   
+    #     def run
+    #       loop do
+    #         # get incoming bytes
+    #         if byte_waiting?
+    #           @io_mutex.synchronize do
+    #             # byte getting code here
+    #             @input_buffer << get_byte
+    #           end
+    #         end
+    #   
+    #         # send outgoing bytes
+    #         until @output_buffer.empty? do
+    #           @io_mutex.synchronize do
+    #             # send a byte
+    #             send_byte( @output_buffer.shift )
+    #           end
+    #         end
+    #       end
+    #     end
+    #   
+    #   end
+    #
     module Interface
-      # nothing here yet
-      # all the action is in LocoBufferUSB.rb
     end
+    
   end
 end
